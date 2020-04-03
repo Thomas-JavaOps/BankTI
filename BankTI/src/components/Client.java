0a1d610fc11a3e77cf92c836d200b11b4962646f
Thomas Isoardo <thomas.isoardo@gmail.com>
2020-04-03 20:01:04 +0200

    Première classe Client

---
+++ BankTI/src/components/Client.java
@@ -0,0 +1,55 @@
+package components;
+
+public class Client {
+
+///////////////////////Initialisation des variables///////////////////////
+	private String name = "Invalide";
+	private String firstName = "Invalide";
+	private int num = 0;
+	private static int numClient;
+
+///////////////////////////Les constructeurs///////////////////////////////	
+	Client() {
+	}
+
+	Client(String name, String firstName) {
+		this.name = name;
+		this.firstName = firstName;
+		++numClient;
+		this.num = numClient;
+		
+	}
+
+////////////////////////On définit les Getters////////////////////
+	public String getName() {
+		return this.name;
+	}
+
+	public String getFirstName() {
+		return this.firstName;
+	}
+
+	public int getNumClient() {
+		return this.num;
+	}
+
+//On définit les Setters
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	
+	public void setNum(int num) {
+		this.num = num;
+	}
+
+///////////////////////////////////Méthodes////////////////////
+
+	public String toString() {
+		return "Le client n°" + this.num + " s'appelle " + this.firstName.toUpperCase() + " " + this.name + ".";
+	}
+
+}
