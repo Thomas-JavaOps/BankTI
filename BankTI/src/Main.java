0ffbdabf450b3c1021b21d944910d6f9b771a3b3
Thomas-JavaOps <62234467+Thomas-JavaOps@users.noreply.github.com>
2020-04-06 00:31:04 +0200

    Update Main.java

--- BankTI/src/Main.java
+++ BankTI/src/Main.java
@@ -7,49 +7,7 @@ import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
-//Classe créée juste pour le JAR JSON fonctionne...
+//Classe crÃ©Ã©e juste pour le JAR JSON fonctionne...
 class Main {
 	
-	public static void main(String[] args) 
-    {
-        //JSON parser object to parse read file
-        JSONParser jsonParser = new JSONParser();
-         
-        try (FileReader reader = new FileReader("employees.json"))
-        {
-            //Read JSON file
-            Object obj = jsonParser.parse(reader);
- 
-            JSONArray employeeList = (JSONArray) obj;
-            System.out.println(employeeList);
-             
-            //Iterate over employee array
-            employeeList.forEach( emp -> parseEmployeeObject( (JSONObject) emp ) );
- 
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (ParseException e) {
-            e.printStackTrace();
-        }
-    }
- 
-    private static void parseEmployeeObject(JSONObject employee) 
-    {
-        //Get employee object within list
-        JSONObject employeeObject = (JSONObject) employee.get("employee");
-         
-        //Get employee first name
-        String firstName = (String) employeeObject.get("firstName");    
-        System.out.println(firstName);
-         
-        //Get employee last name
-        String lastName = (String) employeeObject.get("lastName");  
-        System.out.println(lastName);
-         
-        //Get employee website name
-        String website = (String) employeeObject.get("website");    
-        System.out.println(website);
-    }
 }
